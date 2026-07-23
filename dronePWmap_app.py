639fd608dbe2d9e7b934b02a4830471f33e8a69f
Rob Chancia <152319010+roccis@users.noreply.github.com>
2026-07-23 14:34:40 -0400

    initalization

---
+++ dronePWmap_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Streamlit App
+st.set_page_config(page_title="Drone training modules for NYS growers", layout="wide")
+
+# Hardcoded Mapbox token
+MAPBOX_TOKEN = st.secrets["mapbox"]["token"]
+
+# AWS
+s3_client = boto3.client('s3',aws_access_key_id = st.secrets["aws"]["access_key"], aws_secret_access_key = st.secrets["aws"]["secret_key"],region_name = st.secrets["aws"]["region"])
+fs = s3fs.S3FileSystem(key=st.secrets["aws"]["access_key"], secret=st.secrets["aws"]["secret_key"])
+bucket_name = st.secrets["aws"]["bucket_name"]
+p = 'images'
+
+st.title("")
+st.markdown("")
