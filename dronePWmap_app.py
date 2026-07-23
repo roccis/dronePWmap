ef0ecd669e0bdf8d0809ac06f14b6fca9f20cda4
Rob Chancia <152319010+roccis@users.noreply.github.com>
2026-07-23 15:52:35 -0400

    test

--- dronePWmap_app.py
+++ dronePWmap_app.py
@@ -19,9 +19,4 @@ p = 'images'
 st.title("Drone Training Modules for New York State Growers")
 st.markdown("& Vineyard Pruning Weight Case Study")
 
-st.page_link('dronePWmap_app.py', label='Home', icon='🏠')
-st.page_link('pages/1_dronepilotcert.py', label='Drone Pilot Certification', icon='👨‍✈️')
-st.page_link('pages/2_dronesoftwareselect.py', label='Drone Software Selection', icon='💻')
-st.page_link('pages/3_howtoguides.py', label='How-To Guides', icon='📖')
-st.page_link('pages/4_vinesizesensing.py', label='Vine Size Sensing for Crop Load Management', icon='🍇')
-#functions.page_links()
+functions.page_links()
