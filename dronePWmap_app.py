0e717baaaa917742e8ba475eaac1fc0dfbba0280
Rob Chancia <152319010+roccis@users.noreply.github.com>
2026-07-23 15:26:19 -0400

    test

--- dronePWmap_app.py
+++ dronePWmap_app.py
@@ -20,10 +20,11 @@ st.markdown("& Vineyard Pruning Weight Case Study")
 
 def page_links():
     # builds the sidebar menu
-    st.page_link('dronePWmap_app.py', label='Home', icon='🏠')
-    st.page_link('pages/1_dronepilotcert.py', label='Drone Pilot Certification', icon='👨‍✈️')
-    st.page_link('pages/2_dronesoftwareselect.py', label='Drone Software Selection', icon='💻')
-    st.page_link('pages/3_howtoguides.py', label='How-To Guides', icon='📖')
-    st.page_link('pages/4_vinesizesensing.py', label='Vine Size Sensing for Crop Load Management', icon='🍇')
+    with st.sidebar:
+        st.page_link('dronePWmap_app.py', label='Home', icon='🏠')
+        st.page_link('pages/1_dronepilotcert.py', label='Drone Pilot Certification', icon='👨‍✈️')
+        st.page_link('pages/2_dronesoftwareselect.py', label='Drone Software Selection', icon='💻')
+        st.page_link('pages/3_howtoguides.py', label='How-To Guides', icon='📖')
+        st.page_link('pages/4_vinesizesensing.py', label='Vine Size Sensing for Crop Load Management', icon='🍇')
 
 page_links()
