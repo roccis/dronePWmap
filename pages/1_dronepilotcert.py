44ac81119a9320511f0a2aa93851f65a5a7cf98f
Rob Chancia <152319010+roccis@users.noreply.github.com>
2026-07-23 15:44:32 -0400

    test

--- pages/1_dronepilotcert.py
+++ pages/1_dronepilotcert.py
@@ -2,3 +2,5 @@ import streamlit as st
 from pathlib import Path
 
 st.set_page_config(layout="wide")
+
+st.title("Drone Pilot Certification")
--- pages/3_howtoguides.py
+++ pages/3_howtoguides.py
@@ -2,3 +2,5 @@ import streamlit as st
 from pathlib import Path
 
 st.set_page_config(layout="wide")
+
+st.title("How-To Guides")
