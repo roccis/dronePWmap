0f9cdad300bbc318cf64e81a6e7401abf989afb7
Rob Chancia <152319010+roccis@users.noreply.github.com>
2026-07-23 15:38:48 -0400

    test

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ import streamlit as st
 def page_links():
     # builds the sidebar menu
     with st.sidebar:
-        st.page_link('dronePWmap_app.py', label='Home', icon='🏠')
+        #st.page_link('dronePWmap_app.py', label='Home', icon='🏠')
         st.page_link('pages/1_dronepilotcert.py', label='Drone Pilot Certification', icon='👨‍✈️')
         st.page_link('pages/2_dronesoftwareselect.py', label='Drone Software Selection', icon='💻')
         st.page_link('pages/3_howtoguides.py', label='How-To Guides', icon='📖')
